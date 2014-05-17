2799a737b6a0ac2ffbfd80e74624cf675793e12d
Konilax <konilax@gmail.com>
2014-05-17 16:00:08 -0700

    Added Libs and Natives and Creating a Window

--- src/com/konilax/engine/MainComponent.java
+++ src/com/konilax/engine/MainComponent.java
@@ -1,4 +1,46 @@
 package com.konilax.engine;
 
+import com.konilax.engine.graphics.Window;
+
 public class MainComponent {
+
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 600;
+    public static final String TITLE = "Java 3D Engine";
+
+    public MainComponent() {
+
+    }
+
+    public void start() {
+        run();
+    }
+
+    public void stop() {
+
+    }
+
+    public void run() {
+        while(!Window.isCloseRequested()) {
+            render();
+        }
+
+    }
+
+    public void render() {
+        Window.render();
+    }
+
+    public void cleanUp() {
+
+    }
+
+    public static void main(String[] args) {
+        Window.createWindow(WIDTH, HEIGHT, TITLE);
+
+        MainComponent game = new MainComponent();
+
+        game.start();
+    }
+
 }
---
+++ src/com/konilax/engine/graphics/Window.java
@@ -0,0 +1,40 @@
+package com.konilax.engine.graphics;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
+public class Window {
+
+    public static void createWindow(int width, int height, String title) {
+        Display.setTitle(title);
+        try {
+            Display.setDisplayMode(new DisplayMode(width, height));
+            Display.create();
+        } catch (LWJGLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void render() {
+        Display.update();
+    }
+
+    public static boolean isCloseRequested() {
+        return Display.isCloseRequested();
+    }
+
+    public static int getWidth() {
+        return Display.getDisplayMode().getWidth();
+    }
+
+    public static int getHeight() {
+        return Display.getDesktopDisplayMode().getHeight();
+    }
+
+    public static String getTitle() {
+        return Display.getTitle();
+    }
+
+}
